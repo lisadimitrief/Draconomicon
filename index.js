1e2393876f4bce4daa192c2b365a38abeb06dc22
lisadimitrief <84084978+lisadimitrief@users.noreply.github.com>
2023-07-19 17:09:32 +0200

    responsive

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 let header = document.getElementById("head");
 let activePage = document.getElementsByTagName("h1");
-console.log(activePage[0].outerText=="Connexion");
 header.innerHTML= 
     `
         <div id="titre">
@@ -19,23 +18,87 @@ header.innerHTML=
                 <img src="images/ongletFocus.svg" alt="rectangle rouge" class="ongletFocus" ${activePage[0].outerText=='Connexion' ? 'id="focus"' : ''}>
                 <p>Connexion</p>
             </a>
-            <a>
+            <a href="enyclopedie.html">
                 <img src="images/onglet.png" alt="parchemin déchiré" class="longP">
-                <img src="images/ongletFocus.svg" alt="rectangle rouge" class="ongletFocus">
+                <img src="images/ongletFocus.svg" alt="rectangle rouge" class="ongletFocus" ${activePage[0].outerText=='Encyclopédie' ? 'id="focus"' : ''}>
                 <p>Encyclopédie</p>
             </a>
             
-            <a>
+            <a href="blog.html">
                 <img src="images/onglet.png" alt="parchemin déchiré">
-                <img src="images/ongletFocus.svg" alt="rectangle rouge" class="ongletFocus">
+                <img src="images/ongletFocus.svg" alt="rectangle rouge" class="ongletFocus" ${activePage[0].outerText=='Blog' ? 'id="focus"' : ''}>
                 <p>Blog</p>
             </a>
-            <img src="images/icone.png" id="burger">
+            <a id="burger"><img src="images/icone.png"></a>
         </nav>        
     `
+
+///////BURGER///////////////////////////////////////////////////////////////
+
+const boutonD = document.getElementById("burger");
+
+let container = document.createElement("div");
+let modal = document.createElement("div");
+
+document.body.append(container);
+container.append(modal);
+container.setAttribute("id","container");
+modal.setAttribute("id","modal");
+
+
+modal.innerHTML=
+                `
+                    <ul>
+                        <a href="index.html"><li>Acceuil</li></a>
+                        <a href="inscription.html"><li>Inscription</li></a>
+                        <a href="connexion.html"><li>Connection</li></a>
+                        <a href="encyclopdie.html"><li>Encylopédie</li></a>
+                        <a href="blog.html"><li>Blog</li></a>
+                    </ul>
+                `
+
+                
+                
+boutonD.onclick = ()=>boutonD.classList.toggle("active");
+boutonD.onclick = ()=>container.classList.toggle("menuBurger");
+
+//////FOOTER/////////////////////////////////////////////////////////////////////////////
 
 let footer = document.getElementById("foot");
 footer.innerHTML = 
     `
         <h3>Crée par Lisa Dimitrief-Dontcheff</h3>
     `
+
+
+
+
+
+
+// boutonD.addEventListener("click", popup());
+// boutonD.addEventListener("dblclick", poplus());
+// container.style.display="none";
+// function popup(){
+//     console.log(container);
+//         container.style.display="block";
+//         boutonD.style.position= "fixed";
+//         boutonD.style.bottom= 5+"em";
+//         boutonD.style.top= none;
+//     };
+//     function poplus(){
+//         console.log(container);
+//         container.style.display="none";
+//         boutonD.style.bottom= none;
+//         boutonD.style.top= 0;
+//     };
+//     // function test() {
+//     // if (container.style.display="block"?) {
+//     //     // container.style.display="none";
+//     //     // boutonD.style.bottom= none;
+//     //     // boutonD.style.top= 0;
+//     // } else {
+//     //     container.style.display="block";
+//     //     boutonD.style.position= "fixed";
+//     //     boutonD.style.bottom= 5+"em";
+//     // }
+//     // } 
