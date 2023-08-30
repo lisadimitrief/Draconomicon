c1a7d93e43d27702ce96b7b05a5cf7ae36e441f8
lisadimitrief <84084978+lisadimitrief@users.noreply.github.com>
2023-08-30 16:21:50 +0200

    Minimum
    ajout :
    - ERREURS
    - PWA
    - Server Front

---
+++ API-Draconomicon/src/main/java/com/draconomicon/api/config/SecurityConfig.java
@@ -0,0 +1,50 @@
+package com.draconomicon.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+
+public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain apiSecurity(HttpSecurity http) throws Exception {
+        http
+                .cors((cors) -> cors.configurationSource(request -> {
+                    CorsConfiguration config = new CorsConfiguration();
+                    config.addAllowedOrigin("*"); // Allow all origins. You can restrict it to specific origins if
+                                                  // needed.
+                    config.addAllowedMethod("*"); // Allow all HTTP methods.
+                    config.addAllowedHeader("*"); // Allow all headers.
+                    return config;
+                }))
+                .csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/blog/viewall").permitAll()
+                        .requestMatchers("/blog/search/{username}").permitAll()
+                        .requestMatchers("/login").permitAll()
+                        .requestMatchers("/encyclopedie").permitAll()
+                        .requestMatchers("/register").permitAll()
+                        .requestMatchers("/admin").hasRole("ADMIN")
+                        .anyRequest()
+                        .authenticated())
+                .sessionManagement((auth) -> auth
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        return http.build();
+    }
+}
