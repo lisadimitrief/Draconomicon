ef9a83ffd701f951c3ba45b1b8322cfe947d5274
Lisa_Dimitrief-Dontcheff <lisou-lisa_diana@hotmail.fr>
2023-09-03 18:11:49 +0200

    stade sans erreur*
    * sauf la notfound pour app.js

--- app.js
+++ app.js
@@ -13,25 +13,25 @@ app.use(express.static('public'));
 app.use(morgan("dev"));
 
 app.get('/blog', function (req, res) {
-  res.status(200).sendFile('blog.html', {root:__dirname})
+  res.status(200).sendFile('HTML/blog.html', {root:__dirname})
 })
 app.get('/cgu', function (req, res) {
-  res.status(200).sendFile('cgu.html', {root:__dirname})
+  res.status(200).sendFile('HTML/cgu.html', {root:__dirname})
 })
 app.get('/connexion', function (req, res) {
-  res.status(200).sendFile('connexion.html', {root:__dirname})
+  res.status(200).sendFile('HTML/connexion.html', {root:__dirname})
 })
 app.get('/encyclopedie', function (req, res) {
-  res.status(200).sendFile('encyclopedie.html', {root:__dirname})
+  res.status(200).sendFile('HTML/encyclopedie.html', {root:__dirname})
 })
 app.get('/home', function (req, res) {
-  res.status(200).sendFile('index.html', {root:__dirname})
+  res.status(200).sendFile('HTML/index.html', {root:__dirname})
 })
 app.get('/inscription', function (req, res) {
-  res.status(200).sendFile('inscription.html', {root:__dirname})
+  res.status(200).sendFile('HTML/inscription.html', {root:__dirname})
 })
 app.get('/profil', function (req, res) {
-  res.status(200).sendFile('profil.html', {root:__dirname})
+  res.status(200).sendFile('HTML/profil.html', {root:__dirname})
 })
 app.get('/', function (req, res) {
     res.status(304).redirect('/home')
@@ -39,7 +39,7 @@ app.get('/', function (req, res) {
 
 
 app.use((req, res)=>{
-  res.status(404).sendFile('404.html', {root:__dirname})
+  res.status(404).sendFile('HTML/404.html', {root:__dirname})
 })
 
 app.listen(port, ()=>{
